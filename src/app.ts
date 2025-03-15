25d54b830768def84fee1f8e7358af673094509e
iamanacarolinarezende <pluck_carol@hotmail.com>
2025-03-15 15:38:37 -0400

    Refactoring responsibilities

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import * as http from "http";
+import {getFilterEpisodes, getListEpisodes} from "./controllers/podcasts-controller";
+import { Routes } from "./routes/routes";
+import { HttpMethod } from "./utils/http-methods";
+
+export const app = async (request: http.IncomingMessage , response: http.ServerResponse) => {
+
+    //query string
+    //http://localhost:3333/api/episode?p=PODCASTNAME
+    const[baseUrl, queryString] = request.url?.split("?") ?? ["", ""];
+
+    //console.log(baseUrl);
+    //console.log(queryString);
+
+    //list Podcast
+    if (request.method === HttpMethod.GET && baseUrl === Routes.LIST) {
+        await getListEpisodes(request, response);
+    }
+
+    //filter Podcast
+    if (request.method === HttpMethod.GET && baseUrl === Routes.EPISODE){
+        await getFilterEpisodes(request, response);
+    }
+};
--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,7 @@
 import * as http from "http";
-import {getFilterEpisodes, getListEpisodes} from "./controllers/podcasts-controller";
-import { Routes } from "./routes/routes";
-import { HttpMethod } from "./utils/http-methods";
+import { app } from "./app";
 
-const server = http.createServer(
-    async (request: http.IncomingMessage , response: http.ServerResponse) => {
-
-        //query string
-        //http://localhost:3333/api/episode?p=PODCASTNAME
-        const[baseUrl, queryString] = request.url?.split("?") ?? ["", ""];
-
-        //console.log(baseUrl);
-        //console.log(queryString);
-
-        //list Podcast
-        if (request.method === HttpMethod.GET && baseUrl === Routes.LIST) {
-            await getListEpisodes(request, response);
-        }
-
-        //filter Podcast
-        if (request.method === HttpMethod.GET && baseUrl === Routes.EPISODE){
-            await getFilterEpisodes(request, response);
-        }
-    }
-);
-
+const server = http.createServer(app);
 const port = process.env.PORT;
 
 server.listen(process.env.PORT, () => {
